3d3b2164e2d11db839c2ed0af0c83ab0a561e3b0
DiegoPiza <dbuitra2@gmail.com>
2025-01-23 18:18:30 -0500

    progress update

--- Exploratory_script.R
+++ Exploratory_script.R
@@ -1,19 +1,23 @@
+
+
+# Import Libraries --------------------------------------------------------
 library(tidyverse)
 library(rhdf5)
 
-
-## Importing TFR and visualizing
-
+# Importing TFR  --------------------------------------------------------
 
 # Define the file path
 file_path <- "C:/Users/Data Acquistion/OneDrive - The University of Western Ontario/PhD_Projects/Phase_reset/Results/TFR/Celestine/20190508/TFR_wavelet.mat"
 
-
 # Inspect the file structure
 #h5ls(file_path)
 
-# Read a specific dataset
-data <- h5read(file_path, "TFRwavelet")
+# Read pre-computed wavelet Time frequency representation for example session
+data_structure <- h5read(file_path, "TFRwavelet")
+
+# Parsing and visualizing  --------------------------------------------------------
+TFR<- data_structure$powspctrm
+Freq<- data_structure$
 
 
 
