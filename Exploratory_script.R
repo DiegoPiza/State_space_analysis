33a20cff425f1ee4453352ddf5789e24ec660fc2
DiegoPiza <dbuitra2@gmail.com>
2025-01-23 18:23:01 -0500

    progress update

--- Exploratory_script.R
+++ Exploratory_script.R
@@ -3,10 +3,15 @@
 # Import Libraries --------------------------------------------------------
 library(tidyverse)
 library(rhdf5)
+library(ggplot2)
+library(gapminder) # dataset
+library(finalfit)
+library(plotly)
+library(R.matlab)
 
-# Importing TFR  --------------------------------------------------------
+# Importing TFR and LFP  --------------------------------------------------------
 
-# Define the file path
+# Define the file path for TFR
 file_path <- "C:/Users/Data Acquistion/OneDrive - The University of Western Ontario/PhD_Projects/Phase_reset/Results/TFR/Celestine/20190508/TFR_wavelet.mat"
 
 # Inspect the file structure
@@ -15,9 +20,21 @@ file_path <- "C:/Users/Data Acquistion/OneDrive - The University of Western Onta
 # Read pre-computed wavelet Time frequency representation for example session
 data_structure <- h5read(file_path, "TFRwavelet")
 
+#Define LFP path
+LFP_file_path <-"//martinezsrv.robarts.ca/martinez_data$/Diego Buitrago-Piza/Data/Raw/Celestine/20190508/0508_NSP_corrected.mat"
+#read LFP .mat
+mat_data <- readMat(LFP_file_path)
+LFP=mat_data[["NS2"]][[2]]
 # Parsing and visualizing  --------------------------------------------------------
 TFR<- data_structure$powspctrm
-Freq<- data_structure$
+Freq<- data_structure$freq
+Time<- data_structure$time
+glimpse(TFR)
 
-
-
+#Plotting parameters
+time_plot_range=1:10000 # in seconds
+channel=1
+Frequency=5
+p <- plot_ly(x=Time[time_plot_range],y = TFR[channel,Frequency,time_plot_range], type = 'scatter', mode = 'markers')
+p
+s
