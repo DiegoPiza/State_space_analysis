daa07aad86f057d9938fe6b8f0dbba615b9c0ce8
DiegoPiza <dbuitra2@gmail.com>
2025-02-04 16:08:07 -0500

    create file

---
+++ seeg_preprocessing.R
@@ -0,0 +1,10 @@
+# Import Libraries --------------------------------------------------------
+library(tidyverse)
+library(rhdf5)
+library(ggplot2)
+library(gapminder) # dataset
+library(finalfit)
+library(plotly)
+library(R.matlab)
+# Importing LFP  --------------------------------------------------------
+file_path <- "C:/Users/Data Acquistion/OneDrive - The University of Western Ontario/PhD_Projects/Phase_reset/Results/TFR/Celestine/20190508/TFR_wavelet.mat"
